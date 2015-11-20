b79616bf93a1069b7ac79160d7255e213f5463e5
Jordan Skole <jordan@usebenchmark.com>
2015-11-20 11:21:59 -0500

    selectors are finished w/o dynamic content

--- src/app/matchups/ctrl.matchups.js
+++ src/app/matchups/ctrl.matchups.js
@@ -10,6 +10,12 @@
 
     var vm = this;
 
+    // TODO: Load this information dynamically using the
+    //       Edmunds.com API
+    vm.makes  = ["Ford", "Chevrolet", "Audi", "GMC"];
+    vm.years  = ["2015", "2016"];
+    vm.models = ["Terrain", "Corvette", "A4"];
+
   }
 
 })();
