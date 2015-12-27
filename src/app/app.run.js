1951007476ed8df29e4318efba122a14a7766318
Jordan Skole <jordan@usebenchmark.com>
2015-12-27 12:42:17 -0500

    fix $rootScope.$on issue with a patch

--- src/app/app.run.js
+++ src/app/app.run.js
@@ -11,9 +11,20 @@
     var vm = new Object;
     $log.debug('runBlock end');
 
+    // TODO: Why isn't this working?
+    // vm.on = $rootScope.$on();
+    // vm.on("$stateChangeError", function (event, toState, toParams, fromState, fromParams, error) {
+    //   // throw the error
+    //   $log.error(error);
     //
-    vm.on = $rootScope.$on();
-    vm.on("$stateChangeError", function (event, toState, toParams, fromState, fromParams, error) {
+    //   // We can catch the error thrown when the $requireAuth promise is rejected
+    //   // and redirect the user back to the home page
+    //   if (error === "AUTH_REQUIRED") {
+    //     $state.go("login");
+    //   }
+    // });
+
+    $rootScope.$on('$stateChangeError', function (event, toState, toParams, fromState, fromParams, error) {
       // throw the error
       $log.error(error);
 
@@ -25,7 +36,6 @@
     });
 
     // TODO: Why isn't this working?
-
     // vm.on("$stateChangeStart", function (event, toState, toParams, fromState, fromParams, error) {
     //   $mdToast.cancel();
     // });
